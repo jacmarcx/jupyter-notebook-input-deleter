31ef1f53e8decbb06dac4ef58badd01509a22938
Jacques Marcoux <29962430+jacmarcx@users.noreply.github.com>
2021-12-07 21:16:47 -0600

    added stripper for output cell id

--- jupyter-input-deleter.py
+++ jupyter-input-deleter.py
@@ -7,8 +7,13 @@ def jupyter_input_deleter(filename):
 
     from bs4 import BeautifulSoup
     soup = BeautifulSoup(open(filename),'html.parser')
-
+    
+    #strip input code
     for div in soup.find_all('div',attrs={'class':'input'}):
+        div.decompose()
+    
+    #strip output box cell id number
+    for div in soup.find_all('div',attrs={'class':'prompt output_prompt'}):
         div.decompose()
     
     html_file = open(f'{filename[0:-5]}-codeless.html', 'w')
